68b6ff25cf783507e98e927d4ffdf9924206ff9f
joshua Aisien <97232685+JoshuaAisien@users.noreply.github.com>
2024-03-28 12:47:20 -0100

    Create bujo-is_digit.c
    monty_language

---
+++ bujo-is_digit.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * bujo_isdigit - Entry point
+ *
+ * Return: if is digit  or not, return 1 else 0
+ */
+int bujo_isdigit(void)
+{
+	int i = 0;
+
+	if (global_variable.int_input[0] == '-' || global_variable.int_input[0] == '+')
+		i++;
+	while (global_variable.int_input)
+	{
+		if ((global_variable.int_input[i] >= '0' && (global_variable.int_input[i + 1] >= '0'
+						|| global_variable.int_input[i + 1] == '\0')) &&
+				(global_variable.int_input[i] <= '9' && (global_variable.int_input[i + 1] <= '9'
+												 || global_variable.int_input[i + 1] == '\0')))
+			return (1);
+		else
+			return (0);
+		i++;
+	}
+	return (0);
+}
